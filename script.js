4b0dacf65a23d2f55bc2d6e0b8d25be15c60674a
Isabella <ispe0001@stud.kea.dk>
2024-08-27 17:14:16 +0200

    kategori redigeringer

---
+++ script.js
@@ -0,0 +1,15 @@
+
+window.addEventListener("load", skygge);
+
+function skygge (){
+document.querySelector(".kategoriliste").addEventListener("mouseover", hideshadow);
+}
+
+function hideshadow(){
+    document.querySelector(".kategoriliste").classList.add("hideshadow");
+    document.querySelector(".kategoriliste").addEventListener("mouseout", skyggeigen);
+}
+
+function skyggeigen() {
+    document.querySelector(".kategoriliste").classList.remove("hideshadow")
+}
